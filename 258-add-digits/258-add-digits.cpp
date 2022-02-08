b821313b0dc676dc602477abd20c360b77f34b3f
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-02-08 10:33:55 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int addDigits(int num) {
-       if(num<10)
-           return num;
-        else
-            return addDigits(addDigits(num/10)+num%10);
+        if(num==0)
+            return 0;
+        if(num%9==0)
+            return 9;
+        return num%9;
     }
 };
