bc7d160e993d5c506ec834babfa1679bcd938e4b
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-02-09 02:19:13 +0530

    Time: 774 ms, Memory: 87.2 MB - LeetHub

--- maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
@@ -10,7 +10,11 @@ public:
             //already calculated
             return dp[i][j];
         int op1=mul[i]*nums[j]+ solve(i+1,j+1,nums,mul);
+        //taking 1st element and multiplying for finding solution.
+        //Adding solution excluding 1st element of nums and mul as it has been already used 
         int op2=mul[i]*nums[nums.size()-1-(i-j)]+solve(i+1,j,nums,mul);
+        //taking last element and multiplying for finding solution.
+        //adding solution excluding lsst element of nums and 1st eleemnt of muls as it has been already used 
         return dp[i][j]=max(op1,op2);
     }
     
