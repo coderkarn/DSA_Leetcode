f0db03a0f75e81bd055946d9ffca88083132aaf4
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-02-13 13:12:30 +0530

    Time: 9 ms (20.71%), Space: 7.7 MB (57.94%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-vector<int>ans;
-    map<int,int>mp;
-    for(int i=0;i<arr1.size();i++){
-        mp[arr1[i]]++;
-    }
-    
-    for(int i=0;i<arr2.size();i++){
-        int num=mp[arr2[i]];
-        while(num>=1){
-            ans.push_back(arr2[i]);;
-            num--;
-            mp[arr2[i]]--;
+        map <int,int> m;
+        for(auto i: arr1)
+            m[i]++;
+        vector <int> ans;
+        for(int i=0;i<arr2.size();i++){
+            int fre=m[arr2[i]];
+            while(fre--){
+                ans.push_back(arr2[i]);
+                m[arr2[i]]--;
+            }
         }
-    }
-    for(auto i:mp){
-        if(i.second>0){
+    for(auto i:m){
+        if(i.second>0)
+        {
             int num=i.second;
             while(num>=1){
-            ans.push_back(i.first);;
-            num--;
-            i.second--;
+                ans.push_back(i.first);
+                num--;
+                i.second--;
+                }
+            }
         }
-    }
-    }
-    return ans;
+        return ans;
     }
 };
