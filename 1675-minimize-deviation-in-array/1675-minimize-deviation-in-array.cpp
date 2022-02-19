b4352c335c5ce6b9413212e547631c2593f39f86
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-02-19 13:12:59 +0530

    Time: 258 ms (67.55%), Space: 71.3 MB (83.44%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -3,21 +3,26 @@ public:
     int minimumDeviation(vector<int>& nums) {
         int n=nums.size();
         priority_queue<int> pq;
-        int mi=INT_MAX;//for finding minimum element in nums
+        int mi=INT_MAX;
         
         for(int i=0;i<n;i++){
+            // changing odd to even and also storing to the priority_queue.
             if(nums[i]%2!=0)
                 nums[i]*=2;
             pq.push(nums[i]);
-            
+            // finding minimum element 
             mi=min(nums[i],mi);
         }
         int ans=INT_MAX;
+        //always find max untill max/2!=0
         while(pq.top()%2==0){
+            // removing the max element from the priority queue and finding the diff.
             int ma=pq.top();
             pq.pop();
             ans=min(ans,ma-mi);
+            //updating the minimum.
             mi=min(mi,ma/2);
+            //pushing max/2 to the MAX-HEAP.
             pq.push(ma/2);
         }
         return min(ans,pq.top()-mi);
