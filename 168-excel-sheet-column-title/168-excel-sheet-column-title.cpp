0b6cdab3d558f6ea73b209033ed5dbcfd662af1a
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-06-12 14:40:10 +0530

    Time: 5 ms (7.27%), Space: 6 MB (45.97%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -3,16 +3,11 @@ public:
     string convertToTitle(int colNum) {
         string ans="";
         while(colNum){
+            colNum--;
             int r=colNum%26;
-            if(r==0){
-                ans="Z"+ans;
-                colNum=colNum/26-1;
-            }
-            else{
-                char c='A'+r-1;
-                ans=c+ans;
-                colNum/=26;
-            }
+            char c='A'+r;
+            ans=c+ans;
+            colNum/=26;
         }
         return ans;
     }
