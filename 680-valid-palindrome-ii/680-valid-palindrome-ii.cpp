7cd3bcbc710389fcbb816eb48d7a66f8c8065cec
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-06-20 18:55:38 +0530

    Time: 95 ms (19.11%), Space: 19.7 MB (76.07%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     bool isPal(string &s,int i,int j){
-         while(i<j){
+        while(i<j){
             if(s[i]!=s[j])
-                return 0;
-                i++;
-                j--;       
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
-        return 1;
-}
     bool validPalindrome(string s) {
-        int i=0;
-        int j=s.size()-1;
-        
+        int n=s.size();
+        int i=0,j=n-1;
         while(i<j){
             if(s[i]!=s[j]){
-                return isPal(s,i,j-1)||isPal(s,i+1,j);
+                return isPal(s,i+1,j)||isPal(s,i,j-1);
             }
             i++;
             j--;
         }
-       return 1; 
+        return true;
     }
 };
