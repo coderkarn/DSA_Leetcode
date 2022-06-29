8552fe8efdf2a3c8f89dfddc705351861d9a3647
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-06-29 19:47:55 +0530

    Added solution - LeetHub

--- Power of 2 - GFG/power-of-2.cpp
+++ Power of 2 - GFG/power-of-2.cpp
@@ -12,7 +12,9 @@ class Solution{
     public:
     // Function to check if given number n is a power of two.
     bool isPowerofTwo(long long n){
-        return (n!=0)&&((n&(n-1))==0);
+        if(n==0)
+        return false;
+        return ((n&(n-1))==0);
         // Your code here    
         
     }
