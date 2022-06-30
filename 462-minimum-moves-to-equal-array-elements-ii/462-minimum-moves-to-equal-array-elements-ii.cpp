fc79ae6e8ae474789210bf9678b61be990451aec
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-06-30 13:08:26 +0530

    Time: 27 ms (9.46%), Space: 10.9 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -5,15 +5,11 @@ public:
         sort(nums.begin(),nums.end());
         if(n==1)
             return 0;
-        if(n==2)
-            return abs(nums[1]-nums[0]);
         long long int x=nums[n/2];
-        long long int y=nums[n/2+1];
-        long long int a1=0,a2=0;
+        long long int a1=0;
         for(int i=0;i<n;i++){
             a1+=abs(nums[i]-x);
-            a2+=abs(nums[i]-y);
         }
-        return (int)min(a1,a2);
+        return (int)a1;
     }
 };
