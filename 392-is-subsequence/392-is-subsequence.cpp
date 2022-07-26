428a806055ec50d9efd097fd6faa41d96af7dbcc
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-07-26 09:08:01 +0530

    Time: 5 ms (28.30%), Space: 6.2 MB (88.76%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        /* Alternate: DP (Solution)
+        Find length of LCS of S and T 
+        if it is equal to s then true else false
+        */
+        
         int n=s.size();
         int j=0;
         for(int i=0;i<t.size();i++)
