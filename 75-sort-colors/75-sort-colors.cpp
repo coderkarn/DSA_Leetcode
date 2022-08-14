6d0c154aeeecd25798566ed0bf11d7a29659c078
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-08-15 01:20:05 +0530

    Time: 6 ms (24.15%), Space: 8.4 MB (23.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        //What is Dutch national flag algorithm?
+// One algorithm is to have the top group grow down from the top of the array, the bottom group grow up from the bottom, and keep the middle group just above the bottom. The algorithm indexes three locations, the bottom of the top group, the top of the bottom group, and the top of the middle group.
+
+// Dutch national flag problem
         int n=nums.size();
-        int c1=0,c2=0,c3=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0)
-                c1++;
-            else if(nums[i]==1)
-                c2++;
-            else
-                c3++;
+        int i=0,j=0,k=n-1;
+        while(j<=k){
+            if(nums[j]==0){
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+            else if(nums[j]==2){
+                swap(nums[j],nums[k]);
+                k--;
+            }
+            else{
+                j++;
+            }
         }
-        int j=0;
-        while(c1--)
-            nums[j]=0,j++;
-        while(c2--)
-            nums[j]=1,j++;
-        while(c3--)
-            nums[j]=2,j++;
     }
 };
