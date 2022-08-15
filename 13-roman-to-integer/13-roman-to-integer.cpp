5443817ab10db6ecf73e236f0d361173b9a3eb7e
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-08-15 20:40:34 +0530

    Time: 8 ms (84.03%), Space: 6 MB (64.52%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int sum=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='I'){
+                int j=i+1;
+                if(s[j]=='X'){
+                    sum+=9;  
+                    i++;
+                }
+                else if(s[j]=='V'){
+                    sum+=4;
+                    i++;
+                }else{
+                    sum+=1;
+                }
+            }
+            else if(s[i]=='V'){
+                sum+=5;
+            }
+            else if(s[i]=='L'){
+                sum+=50;
+            }
+            else if(s[i]=='C'){
+                 if(s[i+1]=='M'){
+                    sum+=900;
+                    i++;
+                }else if(s[i+1]=='D'){
+                    sum+=400;
+                    i++;                    
+                }else {
+                    sum+=100;
+                }
+            }  
+            else if(s[i]=='D'){
+                sum+=500;
+            }
+            else if(s[i]=='M'){
+                sum+=1000;
+            }
+            else if(s[i]=='X'){
+                if(s[i+1]=='C'){
+                    sum+=90;
+                    i++;
+                }else if(s[i+1]=='L'){
+                    sum+=40;
+                    i++;                    
+                }else {
+                    sum+=10;
+                }
+            }
+        }
+        return sum;
+    }
+};
