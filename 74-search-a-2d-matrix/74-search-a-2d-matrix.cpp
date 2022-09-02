da3d86a18490e00a0b6e2986372ce7a5c6f9cb3a
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-09-02 10:38:20 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (51.39%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -3,26 +3,38 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size();
         int n=matrix[0].size();
-        for(int i=0;i<m;i++){
-            int s=matrix[i][0];
-            int e=matrix[i][n-1];
-            if(e<target){
-                continue;
-            }
-            else{
-                int j=0;
-                int k=n-1;
-                while(j<=k){
-                    int m=j+(k-j)/2;
-                    if(matrix[i][m]==target)
-                        return true;
-                    else if(matrix[i][m]>target)
-                        k=m-1;
-                    else
-                        j=m+1;
-                }
-            }
+        int i=0,j=n-1;
+        while(i<m && j>=0){
+            if(matrix[i][j]==target)
+                return true;
+            else if(matrix[i][j]>target)
+                j--;
+            else 
+                i++;
         }
-        return false;
+        return 0;
+        // int m=matrix.size();
+        // int n=matrix[0].size();
+        // for(int i=0;i<m;i++){
+        //     int s=matrix[i][0];
+        //     int e=matrix[i][n-1];
+        //     if(e<target){
+        //         continue;
+        //     }
+        //     else{
+        //         int j=0;
+        //         int k=n-1;
+        //         while(j<=k){
+        //             int m=j+(k-j)/2;
+        //             if(matrix[i][m]==target)
+        //                 return true;
+        //             else if(matrix[i][m]>target)
+        //                 k=m-1;
+        //             else
+        //                 j=m+1;
+        //         }
+        //     }
+        // }
+        // return false;
     }
 };
