83be6460cb617f616ff334595a9db06faa82e914
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-09-02 20:28:12 +0530

    Time: 31 ms (52.65%), Space: 19.5 MB (59.92%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //Moore voting algorithm
         int n=nums.size();
-        int x=n/2;
-        unordered_map <int ,int> m;
-        for(auto i: nums)
-            m[i]++;
-        for(auto i: nums){
-            if(m[i]>x)
-                return i;
+        int can=nums[0],votes=1;
+        for(int i=1;i<n;i++){
+            if(votes==0){
+                can=nums[i];
+                votes=1;
+            }
+            else{
+                if(nums[i]==can)
+                    votes++;
+                else
+                    votes--;
+            }
         }
-        return 0;
+        return can;
+        // int n=nums.size();
+        // int x=n/2;
+        // unordered_map <int ,int> m;
+        // for(auto i: nums)
+        //     m[i]++;
+        // for(auto i: nums){
+        //     if(m[i]>x)
+        //         return i;
+        // }
+        // return 0;
     }
 };
