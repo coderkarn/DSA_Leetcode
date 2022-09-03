f3493e9062a7d9a9d2cee3aa078e09cfad10a8df
Saurav Karn <80843032+coderkarn@users.noreply.github.com>
2022-09-03 12:14:42 +0530

    Time: 57 ms (88.45%), Space: 13.2 MB (56.53%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -4,29 +4,36 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
         set <vector <int>> s;
+        vector <vector <int>> ans;
         for(int i=0;i<n-2;i++){
+            if(i == 0 || nums[i] != nums[i-1])
             for(int j=i+1;j<n-1;j++){
-                int new_tar=target-nums[i]-nums[j];
+                long long int new_tar=(long long)target-nums[i]-nums[j];
                 int x=j+1;
                 int y=n-1;
+                if(j == i + 1 || nums[j] != nums[j-1])
                 while(x<y){
-                    int sum=nums[x]+nums[y];
+                    long long int sum=(long long)nums[x]+nums[y];
                     if(sum>new_tar)
                         y--;
                     else if(sum<new_tar)
                         x++;
                     else{
-                        s.insert({nums[i],nums[j],nums[x],nums[y]});
-                        x++;
+                        //s.insert({nums[i],nums[j],nums[x],nums[y]});
+                        ans.push_back({nums[i],nums[j],nums[x],nums[y]});
                         y--;
+                        x++;
+                        while(x < y && nums[x] == nums[x-1])
+                            x++;
+                        
                     }
                 }
                 
             }
         }
-        vector <vector <int>> ans;
-        for(auto z:s)
-            ans.push_back(z);
+        
+        // for(auto z:s)
+        //     ans.push_back(z);
         return ans;
     }
 };
